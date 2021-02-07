ef4507f336006f4c04876ca8dec6c50187757462
christofs <c.schoech@gmail.com>
2021-02-07 17:54:47 +0100

    swapped in setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='coleto',
+    version='0.1.0',
+    date="2021-02-07",
+    author="Christof Schöch"
+    author_email="c.schoech@gmail.com"
+    description="Tool for text comparison."
+    programming_language="Python3"
+    license='MIT Licence',
+    long_description=open('README.md').read(),
+)
